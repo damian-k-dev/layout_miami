6f260abe72cf070c45bf8f698aa8fcd7b999320e
Damian Kurzydlo <duchozywia2024@gmail.com>
2026-03-12 08:54:58 +0000

    feat: implement sliding menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,15 @@
 'use strict';
-
 function handleFormSubmit(e) {
   e.preventDefault();
 }
 
 const btn = document.querySelector('.form-contact__button');
-
 btn.addEventListener('click', handleFormSubmit);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page-content--with-menu');
+  } else {
+    document.body.classList.remove('page-content--with-menu');
+  }
+});
